7d9826ce1cc5e7c825dc240ec5157630290666e2
Stanislas Lange <stanislas.lange@pm.me>
2021-03-19 12:45:57 +0100

    Init Spring boot

---
+++ src/test/java/fr/esgi/beanz/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.beanz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
