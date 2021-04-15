f03cf4b8ccf0b1230145e0583e086d3bf777fc69
Yoni <firroloni.yoni@gmail.com>
2021-04-15 20:19:18 +0200

    remove duplicate dependencies

--- src/main/java/fr/esgi/beanz/api/portfolio/PortfolioController.java
+++ src/main/java/fr/esgi/beanz/api/portfolio/PortfolioController.java
@@ -3,17 +3,19 @@ package fr.esgi.beanz.api.portfolio;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/portfolios")
@@ -34,8 +36,9 @@ public class PortfolioController {
     return portfolio.get();
   }
 
-  @RequestMapping(method = RequestMethod.POST)
-  public String postPortfolio(@Valid @RequestBody Portfolio portfolio) {
+  @PostMapping("/")
+  @ResponseStatus(HttpStatus.CREATED)
+  public String postPortfolio(@Validated @RequestBody Portfolio portfolio) {
 
     final var res = portfolioService.createPortfolio(portfolio);
     return ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString() + '/' + res.getId();
--- src/main/java/fr/esgi/beanz/api/portfolio/dto/CreatePortfolioDTO.java
+++ src/main/java/fr/esgi/beanz/api/portfolio/dto/CreatePortfolioDTO.java
@@ -2,25 +2,20 @@ package fr.esgi.beanz.api.portfolio.dto;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.Valid;
 
 public class CreatePortfolioDTO {
   
   @NotBlank()
   @NotNull()
-  @Valid()
   private String stock;
 
   @NotNull()
-  @Valid()
   private Integer shares;
 
   @NotNull()
-  @Valid()
   private double average;
 
   @NotBlank()
   @NotNull()
-  @Valid()
   private String currency;
 }
