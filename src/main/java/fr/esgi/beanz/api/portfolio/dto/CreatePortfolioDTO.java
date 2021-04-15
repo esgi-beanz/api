10623c9c02abe2b34e740266dc8b7fc77bfd33c5
Yoni <firroloni.yoni@gmail.com>
2021-04-15 14:20:40 +0200

    add valdiator dependency

--- src/main/java/fr/esgi/beanz/api/portfolio/dto/CreatePortfolioDTO.java
+++ src/main/java/fr/esgi/beanz/api/portfolio/dto/CreatePortfolioDTO.java
@@ -1,12 +1,26 @@
 package fr.esgi.beanz.api.portfolio.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
+
 public class CreatePortfolioDTO {
   
+  @NotBlank()
+  @NotNull()
+  @Valid()
   private String stock;
 
+  @NotNull()
+  @Valid()
   private Integer shares;
 
+  @NotNull()
+  @Valid()
   private double average;
 
+  @NotBlank()
+  @NotNull()
+  @Valid()
   private String currency;
 }
