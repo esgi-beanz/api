82eb852b8fde0fe4ffa6af6c2e5c74d35e495a7f
Yoni <firroloni.yoni@gmail.com>
2021-04-18 17:05:57 +0200

    add validation to auth

--- src/main/java/fr/esgi/beanz/api/auth/dto/LoginDTO.java
+++ src/main/java/fr/esgi/beanz/api/auth/dto/LoginDTO.java
@@ -1,10 +1,15 @@
 package fr.esgi.beanz.api.auth.dto;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 
 @Getter
 public class LoginDTO {
+
+    @NotNull(message = "Please provide a username.")
     private String username;
 
+    @NotNull(message = "please provide a password")
     private String password;
 }
