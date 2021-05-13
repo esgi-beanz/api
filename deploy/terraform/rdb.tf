28943789bf465c405d3a6af3658cc4b8454bb955
Stanislas Lange <stanislas.lange@pm.me>
2021-05-13 23:20:01 +0200

    Terraform: deploy k8s cluster, db and regsitry only

--- deploy/terraform/rdb.tf
+++ deploy/terraform/rdb.tf
@@ -1,9 +1,33 @@
+resource "random_password" "admin" {
+  length  = 16
+  special = true
+}
+
+variable "RDB_PASS" {
+  type = string
+}
+resource "scaleway_rdb_user" "api" {
+  instance_id = scaleway_rdb_instance.main.id
+  name        = "api"
+  password    = var.RDB_PASS
+  is_admin    = true
+}
+
 resource "scaleway_rdb_instance" "main" {
-  name           = "jee-db"
-  node_type      = "DB-DEV-S"
+  name           = "beanz-rdb-postgres"
+  node_type      = "db-dev-s"
   engine         = "PostgreSQL-11"
   is_ha_cluster  = false
   disable_backup = true
-  user_name      = "my_initial_user"
-  password       = "thiZ_is_v&ry_s3cret"
+  user_name      = "admin"
+  password       = random_password.admin.result
+}
+
+
+output "rdb_ip" {
+  value = scaleway_rdb_instance.main.endpoint_ip
+}
+
+output "rdb_port" {
+  value = scaleway_rdb_instance.main.endpoint_port
 }
