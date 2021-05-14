b78fb8db1008a9b6f27b454e66be4a92d0cc26ba
Stanislas Lange <stanislas.lange@pm.me>
2021-05-14 09:09:02 +0000

    k8s: update autoscaler_config

--- deploy/terraform/k8s.tf
+++ deploy/terraform/k8s.tf
@@ -5,8 +5,10 @@ resource "scaleway_k8s_cluster" "main" {
   delete_additional_resources = true
 
   autoscaler_config {
-    disable_scale_down         = false
-    scale_down_delay_after_add = "5m"
+    disable_scale_down               = false
+    scale_down_delay_after_add       = "1m"
+    scale_down_unneeded_time         = "1m"
+    scale_down_utilization_threshold = 0.5
   }
 }
 
