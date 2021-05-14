83de2938210a9f638ad826efda50a0cf4fb40c82
Yoni Firroloni <firroloni.yoni@gmail.com>
2021-05-14 14:38:22 +0200

    Add portfolios tests (#9)

---
+++ src/test/java/fr/esgi/beanz/api/portfolios/PortfolioTests.java
@@ -0,0 +1,33 @@
+package fr.esgi.beanz.api.portfolios;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PortfolioTests {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void fetchPortfolioWithoutAuthShouldThrow403Error() throws Exception {
+    this.mockMvc.perform(MockMvcRequestBuilders.get("/portoflios/0"))
+    .andDo(MockMvcResultHandlers.print())
+    .andExpect(MockMvcResultMatchers.status().isForbidden());
+  }
+
+  @Test
+  public void fetchPortfoliosWithoutAuthShouldThrow403Error() throws Exception {
+    this.mockMvc.perform(MockMvcRequestBuilders.get("/portoflios"))
+    .andDo(MockMvcResultHandlers.print())
+    .andExpect(MockMvcResultMatchers.status().isForbidden());
+  }
+
+
+}
